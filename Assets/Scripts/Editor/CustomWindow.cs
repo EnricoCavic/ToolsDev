87880b8e258d3afc95ac30e337dc401444facddc
Enrico Cavicchioli <enricocavic@gmail.com>
2023-02-01 23:35:33 -0300

    Button created

--- Assets/Scripts/Editor/CustomWindow.cs
+++ Assets/Scripts/Editor/CustomWindow.cs
@@ -5,9 +5,21 @@ using UnityEngine;
 
 public class CustomWindow : EditorWindow
 {
+
+    // também é possível criar uma aba nova com essa tag
     [MenuItem("Window/Nova janela customizada", false, 0)]
     public static void OpenWindow()
     {
+        // abre uma nova janela do tipo do parâmetro
         GetWindow(typeof(CustomWindow));
+    }
+
+    private void OnGUI()
+    {
+        if( GUILayout.Button("A wild button appeared"))
+        {
+            Debug.Log("It's pikachu!");
+        }
+        
     }
 }
