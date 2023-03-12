bd0d57db0ab6bed66b564d4b40c012167f08ba3c
Enrico Cavicchioli <enricocavic@gmail.com>
2023-03-12 19:02:59 -0300

    files changet to its own repository

--- Assets/ECTools/Attributes/ReadOnlyAttribute.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace ECTools
-{
-    /// <summary>
-    /// Shows a non interactable object on the inspector. The object must be serialized
-    /// </summary>
-    public class ReadOnlyAttribute : PropertyAttribute { }
-}
